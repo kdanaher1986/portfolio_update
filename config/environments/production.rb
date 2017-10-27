8be49909afdc740e8124e6fb77d7c35668dfe16c
Katherine Danaher <kdanaher1986@gmail.com>
2017-10-26 23:03:10 -0400

    change configuration for images to show in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # keep static images in app for Heroku deployment
+  config.serve_static_assets = true
+  
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
